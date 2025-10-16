85d13b6a8617b8d14d2689a0e96299f75826fdce
shindoo <trofim-dima06@mail.ru>
2025-10-16 16:46:25 +0700

    123

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/olesha/',
+  base: '/olesha/',
   plugins: [
     vue(),
     vueDevTools(),
